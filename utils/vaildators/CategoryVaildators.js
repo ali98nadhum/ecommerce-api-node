9d676eb9cf73b75695078b9d3186cfa2b7717897
Ali nadhum <alinadhum1998@gmail.com>
2025-04-03 14:10:35 +0300

    add updateCategoryValidator

--- utils/vaildators/CategoryVaildators.js
+++ utils/vaildators/CategoryVaildators.js
@@ -11,6 +11,17 @@ exports.createCategoryValidator = [
 ]
 
 
+exports.updateCategoryValidator = [
+    check("title")
+    .optional()
+    .isLength({min: 5}).withMessage("title must be at least 5 characters")
+    .isLength({max: 30}).withMessage("title must be less than 30 characters"),
+    check("id")
+    .isMongoId().withMessage("Invalid id category"),
+    VaildatorMiddleware,
+]
+
+
 exports.deleteCategoryValidator = [
     check("id")
     .isMongoId().withMessage("Invalid id category"),
