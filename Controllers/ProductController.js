1c2a99c38eca687be13a35d60890b15ba691f7a8
Ali nadhum <alinadhum1998@gmail.com>
2025-04-05 21:25:14 +0300

    add createProduct fun

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -16,5 +16,27 @@ const { uploadImageToUploadcare , deleteImageFromUploadcare } = require("../util
 // @access private (only admin)
 // ==================================
 module.exports.createProduct = asyncHandler(async(req , res) => {
-    
+    const {title , description , price , quantity , category , subcategory} = req.body;
+
+    if(!req.file){
+        return res.status(400).json({message: "Image is required"});
+    }
+
+    const {imageUrl , publicId} = await uploadImageToUploadcare(req.file);
+
+    // create product
+    const newProduct = new ProductModel({
+        title,
+        slug: slugify(title),
+        description,
+        price,
+        quantity,
+        category,
+        subcategory,
+        imageCover: {url:imageUrl , publicId}
+    })
+
+    await newProduct.save();
+
+    res.status(201).json({message: 'Product saved successfully' , data:newProduct})
 })
