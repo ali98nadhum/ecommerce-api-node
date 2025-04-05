d3a62b763c2f2b30e230a6b01f01980c87d0a0b9
Ali nadhum <alinadhum1998@gmail.com>
2025-04-05 21:13:24 +0300

    add productModel

---
+++ model/ProductModel.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 30,
+      maxlength: 1000,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      min: [0, "Price must be greater than or equal to 0"],
+      set: (value) => Math.round(value),
+    },
+
+    priceAfterDiscount: {
+      type: Number,
+    },
+
+    quantity: {
+      type: Number,
+      required: true,
+      trim: true,
+      min: 0,
+    },
+
+    imageCover: {
+      url: { type: String },
+      publicId: { type: String },
+    },
+
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CategoryModel",
+      required: true,
+    },
+
+    subcategory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "SubcategoryModel",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const ProductModel = mongoose.model("ProductModel", ProductSchema);
+
+module.exports = {
+  ProductModel,
+};
