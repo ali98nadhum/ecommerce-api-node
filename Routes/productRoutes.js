814d2a30540c691b1a79e48a9901ecbc9373915d
Ali nadhum <alinadhum1998@gmail.com>
2025-04-06 17:47:24 +0300

    add getOneProduct route

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,4 +1,4 @@
-const { createProduct } = require("../Controllers/ProductController");
+const { createProduct, getOneProduct } = require("../Controllers/ProductController");
 const {createProductValidator} = require("../utils/vaildators/ProductVaildators");
 const uploadPhoto = require("../middlewares/multerConfig");
 const router = require("express").Router();
@@ -6,6 +6,11 @@ const router = require("express").Router();
 router
   .route("/")
   .post(uploadPhoto.single("image"),createProductValidator , createProduct)
+
+
+router
+  .route("/:slug")
+  .get(getOneProduct)
 
 
 
