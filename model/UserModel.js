61629849bbaa14c5321aedab668e026a21900ba7
Ali nadhum <alinadhum1998@gmail.com>
2025-04-07 14:41:52 +0300

    add userModel

---
+++ model/UserModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 6,
+    maxlength: 25,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 8,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: [8, "password must be at least 8 characters"],
+  },
+
+  role: {
+    type: String,
+    enum: ["superAdmin", "admin", "user"],
+    default: "user",
+  },
+
+  phone: String,
+});
+
+const UserModel = mongoose.model("UserModel", UserSchema);
+
+module.exports = {
+  UserModel,
+};
