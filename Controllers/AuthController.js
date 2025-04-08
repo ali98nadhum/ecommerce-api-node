efe1789e967650335607a68e5bfa2e7b35cbdf50
Ali nadhum <alinadhum1998@gmail.com>
2025-04-08 21:06:01 +0300

    add login fun

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const { UserModel } = require("../model/UserModel");
 const { hashPassword } = require("../middlewares/hashPassword");
+const bcrypt = require("bcryptjs");
 
 
 
@@ -28,4 +29,39 @@ module.exports.register = asyncHandler(async(req , res) => {
     await newUser.save();
 
     res.status(201).json({message: "Registration successful"})
+})
+
+
+
+
+
+
+
+
+
+
+
+// ==================================
+// @desc Login user
+// @route /api/v1/auth/login
+// @method POST 
+// @access public
+// ==================================
+module.exports.login = asyncHandler(async(req, res) => {
+    const {email , password} = req.body;
+
+    // Check if user exists
+  const user = await UserModel.findOne({email});
+  if(!user){
+    return res.status(400).json({message: "Invalid email or password"})
+  }
+
+    // Check if password matches
+    const isPasswordMatch = await bcrypt.compare(password , user.password);
+    if(!isPasswordMatch){
+      return res.status(400).json({message: "Invalid email or password"})
+    }
+
+
+    res.status(200).json({message:`login_success hi ${user.name}`});
 })
