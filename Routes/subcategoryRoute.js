b4bbccb1c895fabecba6101a732b6e067f043976
Ali nadhum <alinadhum1998@gmail.com>
2025-04-09 22:23:27 +0300

    update subcategory routes

--- Routes/subcategoryRoute.js
+++ Routes/subcategoryRoute.js
@@ -1,31 +1,40 @@
+
 const { createSubcategory, getAllSubcategory, getOneSubcategory, deleteSubcategory, updateSubcategory } = require("../Controllers/subcategoryController");
 const uploadPhoto = require("../middlewares/multerConfig");
 const {createSubCategoryValidator, deleteSubCategoryValidator, updateSubCategoryValidator} = require("../utils/vaildators/SubcategoryVaildators");
 const router = require("express").Router();
+const AuthService = require("../utils/token/AuthService");
+
 
 
 
 router
   .route("/")
-  .post(uploadPhoto.single("image"),createSubCategoryValidator, createSubcategory)
-  .get(getAllSubcategory)
+  .post(
+    AuthService.protect,
+    AuthService.allowedTo("superAdmin", "admin"),
+    uploadPhoto.single("image"),
+    createSubCategoryValidator,
+    createSubcategory
+  )
+  .get(getAllSubcategory);
 
-
-router
-  .route("/:slug")
-  .get(getOneSubcategory)
-
-
+router.route("/:slug").get(getOneSubcategory);
 
 router
   .route("/:id")
-  .delete(deleteSubCategoryValidator , deleteSubcategory)
-  .put(uploadPhoto.single("image") , updateSubCategoryValidator , updateSubcategory)
-
-
-
-
-
-
+  .delete(
+    AuthService.protect,
+    AuthService.allowedTo("superAdmin", "admin"),
+    deleteSubCategoryValidator,
+    deleteSubcategory
+  )
+  .put(
+    AuthService.protect,
+    AuthService.allowedTo("superAdmin", "admin"),
+    uploadPhoto.single("image"),
+    updateSubCategoryValidator,
+    updateSubcategory
+  );
 
 module.exports = router;
