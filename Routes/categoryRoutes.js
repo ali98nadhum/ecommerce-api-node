60e7c986d554b7ab102055bf8be9f16c6489f39d
Ali nadhum <alinadhum1998@gmail.com>
2025-04-09 22:17:23 +0300

    update category route

--- Routes/categoryRoutes.js
+++ Routes/categoryRoutes.js
@@ -1,17 +1,36 @@
-const {createCategory,getOneCateogry,getAllCategories, deleteCategory, updateCategory} = require("../Controllers/categoryController");
-const {createCategoryValidator, deleteCategoryValidator, updateCategoryValidator} = require("../utils/vaildators/CategoryVaildators");
+const {createCategory,getOneCateogry,getAllCategories,deleteCategory,updateCategory} = require("../Controllers/categoryController");
+const {createCategoryValidator,deleteCategoryValidator, updateCategoryValidator,} = require("../utils/vaildators/CategoryVaildators");
 const uploadPhoto = require("../middlewares/multerConfig");
 const router = require("express").Router();
+const AuthService = require("../utils/token/AuthService");
 
 router
   .route("/")
-  .post(uploadPhoto.single("image"), createCategoryValidator, createCategory)
+  .post(
+    AuthService.protect,
+    AuthService.allowedTo("superAdmin", "admin"),
+    uploadPhoto.single("image"),
+    createCategoryValidator,
+    createCategory
+  )
   .get(getAllCategories);
 
 router.route("/:slug").get(getOneCateogry);
 
-router.route("/:id")
-.delete(deleteCategoryValidator , deleteCategory)
-.put(uploadPhoto.single("image"),updateCategoryValidator , updateCategory)
+router
+  .route("/:id")
+  .delete(
+    AuthService.protect,
+    AuthService.allowedTo("superAdmin", "admin"),
+    deleteCategoryValidator,
+    deleteCategory
+  )
+  .put(
+    AuthService.protect,
+    AuthService.allowedTo("superAdmin", "admin"),
+    uploadPhoto.single("image"),
+    updateCategoryValidator,
+    updateCategory
+  );
 
 module.exports = router;
