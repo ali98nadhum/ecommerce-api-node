81fbe0c8291b03fff5f2318d368195ce56c0a564
Ali nadhum <alinadhum1998@gmail.com>
2025-04-11 01:33:57 +0300

    add createBrandValidator

---
+++ utils/vaildators/brandVaildators.js
@@ -0,0 +1,17 @@
+const { check } = require("express-validator");
+const VaildatorMiddleware = require("../../middlewares/vaildatorMiddleware");
+const { BrandModel } = require("../../model/BrandModel");
+
+
+
+exports.createBrandValidator = [
+  check("title")
+    .notEmpty()
+    .withMessage("title is required")
+    .isLength({ min: 3 })
+    .withMessage("title must be at least 3 characters")
+    .isLength({ max: 20 })
+    .withMessage("title must be less than 20 characters"),
+
+  VaildatorMiddleware,
+];
