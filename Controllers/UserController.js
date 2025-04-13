d96ea7e3240b37977a69f3f44447cb1903fbac5c
Ali nadhum <alinadhum1998@gmail.com>
2025-04-13 18:49:32 +0300

    add getAllUsers fun

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -2,6 +2,21 @@ const asyncHandler = require("express-async-handler");
 const { UserModel } = require("../model/UserModel");
 
 
+
+// ==================================
+// @desc Get All users
+// @route /api/v1/user/
+// @method GET
+// @access private (only admin)
+// ==================================
+module.exports.getAllUsers = asyncHandler(async(req , res) => {
+  const users = await UserModel.find({})
+  res.status(200).json({data: users})
+})
+
+
+
+
 // ==================================
 // @desc update user role
 // @route /api/v1/user/:id
