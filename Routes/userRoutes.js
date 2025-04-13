d6e3aa043d5af99a838f87d8de5ed5c3787e4da5
Ali nadhum <alinadhum1998@gmail.com>
2025-04-13 19:06:56 +0300

    add getUser route

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,5 +1,5 @@
-const { deleteAccount, updateUserRole, getAllUsers } = require("../Controllers/UserController");
-const {deleteAccountValidator, updateUserRoleValidator,} = require("../utils/vaildators/AuthVaildators");
+const { deleteAccount, updateUserRole, getAllUsers, getUser } = require("../Controllers/UserController");
+const {deleteAccountValidator, updateUserRoleValidator, getUserValidator,} = require("../utils/vaildators/AuthVaildators");
 const AuthService = require("../utils/token/AuthService");
 
 const router = require("express").Router();
@@ -16,6 +16,12 @@ router
     AuthService.allowedTo("superAdmin"),
     updateUserRoleValidator,
     updateUserRole
+  )
+  .get(
+    AuthService.protect,
+    AuthService.allowedTo("superAdmin" , "admin"),
+    getUserValidator,
+    getUser
   )
 
  router
