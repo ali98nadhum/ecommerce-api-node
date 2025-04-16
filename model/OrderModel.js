cc71b3e66c7183322991448d58e070a9310c1291
Ali nadhum <alinadhum1998@gmail.com>
2025-04-16 16:59:10 +0300

    add orderModel

---
+++ model/OrderModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserModel",
+      required: true,
+    },
+
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "ProductModel",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+
+    totalPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    orderStatus: {
+      type: String,
+      enum: ["pending", "confirmed", "cancelled"],
+      default: "pending",
+    },
+
+    deliveryStatus: {
+      type: String,
+      enum: ["delivered", "cancelled", "not_shipped", "in_delivery"],
+      default: "not_shipped",
+    },
+
+    shippingAddress: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const OrderModel = mongoose.model("OrderModel", OrderSchema);
+
+module.exports = {
+  OrderModel,
+};
