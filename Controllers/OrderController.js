8c4321888ea514afae171f373c51bd08e222dfab
Ali nadhum <alinadhum1998@gmail.com>
2025-04-17 11:33:04 +0300

    update deleteOrder fun

--- Controllers/OrderController.js
+++ Controllers/OrderController.js
@@ -120,5 +120,8 @@ module.exports.createOrder = asyncHandler(async(req , res) => {
 // ==================================
 module.exports.deleteOrder = asyncHandler(async(req , res) => {
   const order = await OrderModel.findOneAndDelete(req.params.id);
+  if(!order){
+    return res.status(404).json({message: `not found order for this id ${req.params.id}`})
+  }
   res.status(200).json({message: "order deleted Successfully"})
 })
