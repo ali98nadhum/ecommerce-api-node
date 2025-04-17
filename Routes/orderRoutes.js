a2b8dbcf5d07cbeb5fa589382d3fee82a946c667
Ali nadhum <alinadhum1998@gmail.com>
2025-04-17 11:11:18 +0300

    update order route

--- Routes/orderRoutes.js
+++ Routes/orderRoutes.js
@@ -1,12 +1,13 @@
 const { createOrder, getAllOrder } = require("../Controllers/OrderController");
 const AuthService = require("../utils/token/AuthService");
+const { createOrderValidator } = require("../utils/vaildators/OrderVaildators");
 const router = require("express").Router();
 
 
 
   router
   .route("/")
-  .post(AuthService.protect , createOrder)
+  .post(AuthService.protect,createOrderValidator , createOrder)
   .get(AuthService.protect , AuthService.allowedTo("superAdmin" , "admin") , getAllOrder)
 
 
