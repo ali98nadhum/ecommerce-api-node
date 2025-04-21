0a3fe3523c295c6765c203176669ef3de53346b0
Ali nadhum <alinadhum1998@gmail.com>
2025-04-21 04:32:49 +0300

    update createOrder fun

--- Controllers/OrderController.js
+++ Controllers/OrderController.js
@@ -66,56 +66,63 @@ module.exports.getOneOrder = asyncHandler(async(req , res) => {
 // @method POST
 // @access private (just for login user)
 // ==================================
-module.exports.createOrder = asyncHandler(async(req , res) => {
+module.exports.createOrder = asyncHandler(async (req, res) => {
+  const { products, shippingAddress } = req.body;
 
-    const {products , shippingAddress} = req.body;
+  let totalPrice = 0;
 
-    let totalPrice = 0;
+  for (const item of products) {
+    const product = await ProductModel.findById(item.product);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Product not found: ${item.product}` });
+    }
 
-    for (const item of products) {
-        const product = await ProductModel.findById(item.product);
-        if (!product) {
-          return res.status(404).json({ message: `Product not found: ${item.product}` });
-        }
-      
-        // check if quantity product is found
-        if (product.quantity < item.quantity) {
-          return res.status(400).json({
-            message: `Insufficient stock for product: ${product.title}`,
-            available: product.quantity,
-            requested: item.quantity
-          });
-        }
-      
-         // count total price
-         totalPrice += product.priceAfterDiscount? product.priceAfterDiscount * item.quantity : product.price * item.quantity
-      }
-
-    //   generate Code for order
-      const orderCode = await generateOrderCode(req.user.username);
-      
-
-      const newOrder = new OrderModel({
-        user: req.user.id,
-        products,
-        totalPrice,
-        shippingAddress,
-        orderCode
+    // check if quantity product is found
+    if (product.quantity < item.quantity) {
+      return res.status(400).json({
+        message: `Insufficient stock for product: ${product.title}`,
+        available: product.quantity,
+        requested: item.quantity,
       });
-  
-      await newOrder.save();
+    }
 
-      // update quantity in db
-      for (const item of products) {
-        await ProductModel.findByIdAndUpdate(
-          item.product,
-          { $inc: { quantity: -item.quantity } },
-          { new: true }
-        );
-      }
+    // count total price
+    totalPrice += product.priceAfterDiscount
+      ? product.priceAfterDiscount * item.quantity
+      : product.price * item.quantity;
+  }
 
-      res.status(201).json({ message: "Order created successfully", data: newOrder });
-})
+  //   generate Code for order
+  const orderCode = await generateOrderCode(req.user.username);
+
+  const newOrder = new OrderModel({
+    user: req.user.id,
+    products,
+    totalPrice,
+    shippingAddress,
+    orderCode,
+  });
+
+  await newOrder.save();
+
+  // update quantity in db
+  for (const item of products) {
+    const updatedProduct = await ProductModel.findByIdAndUpdate(
+      item.product,
+      { $inc: { quantity: -item.quantity } },
+      { new: true }
+    );
+
+    if (updatedProduct && updatedProduct.quantity === 0) {
+      updatedProduct.isAvailable = false;
+      await updatedProduct.save();
+    }
+  }
+
+  res.status(201).json({ message: "Order created successfully", data: newOrder });
+});
 
 
 // ==================================
